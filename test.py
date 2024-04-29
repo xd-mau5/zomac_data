dea3b8e65d0fdf9dc86e558a663a64d8bf2f190c
Omar Izquierdo <electromusic1998@gmail.com>
2024-04-29 16:01:42 -0500

    Agregar python-dotenv a los requisitos y utilizar variables de entorno en test.py

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 import dropbox
+import os
+from dotenv import load_dotenv
 
-TOKEN = ''
+load_dotenv()
+
+TOKEN = os.getenv('DROPBOX_TOKEN')
+KEY = os.getenv('DROPBOX_KEY')
+SECRET = os.getenv('DROPBOX_SECRET')
+
+folder = (r'data/Dropbox')
+
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
 dbx = dropbox.Dropbox(TOKEN)
 
-
+print(dbx.files_list_folder(r'/test'))

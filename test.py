16b67d0c01d9a35429e435e553f95dd8dd4d0fa1
Omar Izquierdo <electromusic1998@gmail.com>
2024-04-30 14:50:31 -0500

    Actualizar la función de búsqueda de archivos en test.py

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
 import dropbox
 import os
-from dotenv import load_dotenv, dotenv_values, set_key
+from dotenv import dotenv_values, set_key
 import dropbox.files
 import dropbox.oauth
 
-env_vars = dotenv_values('.env')
-print('Varaibles de entorno actuales:')
+env_vars = dotenv_values(".env")
+print("Varaibles de entorno actuales:")
 for key, value in env_vars.items():
-    print(f'{key}={value}')
+    print(f"{key}={value}")
 
-KEY = env_vars['DROPBOX_KEY']
-SECRET = env_vars['DROPBOX_SECRET']
-folder = (r'data/Dropbox')
-TOKEN = env_vars['DROPBOX_TOKEN']
+KEY = env_vars["DROPBOX_KEY"]
+SECRET = env_vars["DROPBOX_SECRET"]
+folder = r"data/Dropbox"
+TOKEN = env_vars["DROPBOX_TOKEN"]
 dbx = dropbox.Dropbox(TOKEN)
 
+
 def dropbox_oauth():
-    flow = dropbox.DropboxOAuth2FlowNoRedirect(KEY, SECRET, token_access_type='legacy')
+    flow = dropbox.DropboxOAuth2FlowNoRedirect(KEY, SECRET, token_access_type="legacy")
     authorize_url = flow.start()
     print("1. Ir a: " + authorize_url)
     print("2. Click 'Permitir' (iniciar sesión primero).")
@@ -26,45 +27,48 @@ def dropbox_oauth():
     try:
         oauth_result = flow.finish(auth_code)
     except Exception as e:
-        print('Error: %s' % (e,))
+        print("Error: %s" % (e,))
         return
 
     return oauth_result.access_token
 
-def files_download(dbx: dropbox.Dropbox, folder: str, file: str, remote_folder: str = '/test'):
-    with open(os.path.join(folder, file), 'wb') as f:
-        metadata, res = dbx.files_download(path=f'/test/{file}')
+
+def files_download(dbx: dropbox.Dropbox, folder: str, file: str, remote_folder: str):
+    with open(os.path.join(folder, file), "wb") as f:
+        metadata, res = dbx.files_download(remote_folder + "/" + file)
         f.write(res.content)
+
 
 def search_excel_rdt(dbx: dropbox.Dropbox, folder: str, remote_folder: str):
     for entry in dbx.files_list_folder(remote_folder).entries:
-        if entry.name.endswith('.xlsx') and entry.name.startswith('RDT'):
-            files_download(dbx, folder, entry.name)
-            if entry.name.endswith('.xlsx') and entry.name.startswith('RDT'):
-                print(f'Archivo {entry.name} descargado con éxito')
+        if entry.name.endswith(".xlsx") and entry.name.startswith("RDT"):
+            print(f"Archivo {entry.name} encontrado")
+            files_download(dbx, folder, entry.name, remote_folder)
+            if entry.name.endswith(".xlsx") and entry.name.startswith("RDT"):
+                print(f"Archivo {entry.name} descargado con éxito")
 
-# Verificar si el token todavia es valido, si no, solicitar uno nuevo
+
 def check_token(TOKEN: str):
-    print('Verificando token...')
+    print("Verificando token...")
     dbx = dropbox.Dropbox(TOKEN)
-    print('Token recibido en la variable dbx', TOKEN)
+    print("Token recibido en la variable dbx", TOKEN)
     while True:
         try:
-            print('Accediendo a la cuenta de Dropbox...')
+            print("Accediendo a la cuenta de Dropbox...")
             dbx.users_get_current_account()
             break
         except dropbox.exceptions.AuthError as e:
-            print('Token invalido')
+            print("Token invalido")
             TOKEN = dropbox_oauth()
-            # Eliminamos las comillas simples del token
             TOKEN = TOKEN.replace("'", "")
-            set_key('.env', 'DROPBOX_TOKEN', TOKEN)
+            set_key(".env", "DROPBOX_TOKEN", TOKEN)
             dbx = dropbox.Dropbox(TOKEN)
-            print('Token actualizado')
+            print("Token actualizado")
+
 
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_token(TOKEN)
-    search_excel_rdt(dbx, folder, '/test')
+    search_excel_rdt(dbx, folder, "/TROPICAL  2022/Nomina Dopbox/RDT 2023")
